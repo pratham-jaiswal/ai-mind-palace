5a37144097b097811d04e7c25b662b172f5055a8
pratham-jaiswal <prathamj0502@gmail.com>
2025-08-15 23:24:43 +0530

    add cors

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask
+from flask_cors import CORS
 from routes import register_routes
 from config import Config
 from models import db
+from utils.env_vars import ALLOWED_ORIGINS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, supports_credentials=True, origins=ALLOWED_ORIGINS)
     app.config.from_object(Config)
 
     db.init_app(app)
