fc29f7abc39c9bd84b2c56004daae042f9916f0c
pratham-jaiswal <prathamj0502@gmail.com>
2025-08-21 23:55:33 +0530

    add conversation bp to route

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -5,6 +5,7 @@ from controllers.person_controller import bp as people_bp
 from controllers.decision_controller import bp as decisions_bp
 from controllers.chunk_controller import bp as chunks_bp
 from controllers.mind_palace_controller import bp as mind_palace_bp
+from controllers.conversation_controller import bp as conversation_bp
 
 def register_routes(app):
     app.register_blueprint(users_bp)
@@ -14,3 +15,4 @@ def register_routes(app):
     app.register_blueprint(decisions_bp)
     app.register_blueprint(chunks_bp)
     app.register_blueprint(mind_palace_bp)
+    app.register_blueprint(conversation_bp)
